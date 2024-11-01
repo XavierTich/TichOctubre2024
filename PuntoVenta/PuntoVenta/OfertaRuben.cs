8ddba0bb4744c76afc451059a36c6383d175bca3
Rubén <103855234+KidBlue3@users.noreply.github.com>
2024-11-01 17:58:53 -0600

    Oferta Rubén
    Clase OfertaRuben añadida

---
+++ PuntoVenta/PuntoVenta/OfertaRuben.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoVenta
+{
+    internal class OfertaRuben : IArticulo
+    {
+        public string Imprimir()
+        {
+            return "Rubén no supo que poner aquí";
+        }
+    }
+}
