dc8de1585ee7040ad59e22cb73807e05bb4aae97
XavierTich <116765529+XavierTich@users.noreply.github.com>
2024-11-01 17:45:39 -0600

    cambio en la clase program

--- PuntoVenta/PuntoVenta/Program.cs
+++ PuntoVenta/PuntoVenta/Program.cs
@@ -12,7 +12,10 @@ namespace PuntoVenta
         {
             Ticket ticket = new Ticket();
             ticket.listaArticulos.Add(new ArticuloTich());
-            ticket.listaArticulos.Add(new ArticuloXavier());    
+            ticket.listaArticulos.Add(new ArticuloXavier());
+            // ticket.listaArticulos.Add(new ArticuloTich());
+            // ticket.listaArticulos.Add(new ArticuloTich());
+            // ticket.listaArticulos.Add(new ArticuloTich());
             ticket.Imprimir();
             ticket.listaArticulos.Add(new ArticuloJose());
             Console.ReadKey();  
